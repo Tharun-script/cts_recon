f737ae4eec18f632788814c6a351822125316fe0
Tharun-script <tharunconnect05@gmail.com>
2025-09-02 21:50:47 +0530

    report.py

--- report.py
+++ report.py
@@ -9,8 +9,8 @@ def generate_report(domain):
     project_root = os.path.abspath(os.path.dirname(__file__))
     report_dir = os.path.join(project_root, f"{safe_domain}reports")
 
-    final_txt_path = os.path.join(project_root, f"{safe_domain}_final.txt")
-    normalized_json_path = os.path.join(project_root, f"{safe_domain}_normalized.json")
+    final_txt_path = os.path.join(report_dir, f"{safe_domain}_final.txt")
+    normalized_json_path = os.path.join(report_dir, f"{safe_domain}_normalized.json")
 
     combined_data = {}
     human_output = []
@@ -21,7 +21,7 @@ def generate_report(domain):
         return
 
     # Ensure consistent order of module reports
-    order = ["domain.json", "shodan.json", "scraping.json", "bucket.json"]
+    order = ["domain.json", "shodan.json", "scrapping.json", "bucket.json"]
 
     for file in order:
         path = os.path.join(report_dir, file)
